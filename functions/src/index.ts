b4f6b95d9c6b2a9da013b8efc81f45e00132360b
kosaku kurino <kousaku.maron@gmail.com>
2020-10-19 22:02:18 +0900

    feat: add follow and follower counter trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,3 +4,8 @@ admin.initializeApp()
 
 export { createUser } from './triggers/user/create'
 export { createUserCallable } from './handlers/user/create'
+
+export { incrementFollowCount } from './triggers/user/incrementFollowCount'
+export { decrementFollowCount } from './triggers/user/decrementFollowCount'
+export { incrementFollowerCount } from './triggers/user/incrementFollowerCount'
+export { decrementFollowerCount } from './triggers/user/decrementFollowerCount'
